028ccfab880735ea0a9b5dc5d562af5c5ad4baec
Boris Vinogradov <no111u3@gmail.com>
2019-10-30 21:32:08 +0300

    Adding project skeleton

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use panic_itm as _;
+
+use stm32f1xx_hal as _;
+
+use cortex_m::asm::wfi;
+use rtfm::app;
+
+#[app(device = stm32f1xx_hal::pac)]
+const APP: () = {
+    #[init]
+    fn init() {}
+
+    #[idle]
+    fn idle() -> ! {
+        loop {
+            // Waits for interrupt
+            wfi();
+        }
+    }
+};
